3f8847952066377d33f5422c5e7b0087c2041a88
@fixmybugs <fixmybugs@github.com>
2023-02-15 20:06:21 -0600

    making the stopFetchData function work correctly

--- src/baseClient/baseClient.js
+++ src/baseClient/baseClient.js
@@ -3,34 +3,35 @@ import getPokemon from './baseClientMethods/getPokemon.js';
 import getBerry from './baseClientMethods/getBerry.js';
 
 
-export default class baseClient extends eventEmitter{
+export default class baseClient extends eventEmitter {
 
-    constructor(){
+    constructor() {
         super()
-        this.setIntervalId;
-    }
-    
-    async getPokemon({name}){
-        return await getPokemon({name: name});
+        this.pokemonFetcherId;
+        this.berryFetcherId;
     }
 
-    async getBerry({name, fullData}){
-        return await getBerry({name: name, fullData: fullData});
+    async getPokemon({ name, fullData }) {
+        return await getPokemon({ name: name });
+    }
+
+    async getBerry({ name, fullData }) {
+        return await getBerry({ name: name, fullData: fullData });
     }
 
 
-    getPokemonDataInXSeconds({nameList, timeInSeconds}){
+    getPokemonDataInXSeconds({ nameList, timeInSeconds }) {
 
-         //nameList form example: ["pikachu", "charmander", "squirtle"] @array
-         //timeInSeconds  form example: 8  @number
+        //nameList form example: ["pikachu", "charmander", "squirtle"] @array
+        //timeInSeconds  form example: 8  @number
 
-        if(timeInSeconds < 3) throw new Error("{timeInSeconds} must not be less than 3");
+        if (timeInSeconds < 3) throw new Error("{timeInSeconds} must not be less than 3");
 
-        this.setIntervalId = setInterval(async()=>{
+        this.pokemonFetcherId = setInterval(async () => {
 
-            for(let index = 0; index < nameList.length; index ++){
+            for (let index = 0; index < nameList.length; index++) {
 
-                let pokeInfo = await this.getPokemon({name: nameList[index]})
+                let pokeInfo = await this.getPokemon({ name: nameList[index] })
                 this.emit("pokemon_fetched", pokeInfo);
             }
 
@@ -38,28 +39,29 @@ export default class baseClient extends eventEmitter{
 
     }
 
-    getBerryDataInXSeconds({nameList, full, timeInSeconds}){
+    getBerryDataInXSeconds({ nameList, full, timeInSeconds }) {
 
         //nameList form example: ["oran", "sitrus", "lum"] @array
         //timeInSeconds  form example: 8  @number
 
-       if(timeInSeconds < 3) throw new Error("{timeInSeconds} must not be less than 3");
+        if (timeInSeconds < 3) throw new Error("{timeInSeconds} must not be less than 3");
 
-       this.setIntervalId = setInterval(async()=>{
+        this.berryFetcherId = setInterval(async () => {
 
-           for(let index = 0; index < nameList.length; index ++){
+            for (let index = 0; index < nameList.length; index++) {
 
-               let berryInfo = await this.getBerry({name: nameList[index], fullData: full})
-               this.emit("berry_fetched", berryInfo);
-           }
+                let berryInfo = await this.getBerry({ name: nameList[index], fullData: full })
+                this.emit("berry_fetched", berryInfo);
+            }
 
-       }, timeInSeconds * 1000);
+        }, timeInSeconds * 1000);
 
-   }
-
-    stopFetchData(){
-        clearInterval(this.setIntervalId);
     }
 
-}
+    stopFetchData() {
+        clearInterval(this.pokemonFetcherId);
+        clearInterval(this.berryFetcherId);
+    }
 
+
+}
